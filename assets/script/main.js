3da78e47d88f2c8a062e88ecd2d0aba8f6583998
Lorrany Furtado <lorranyfdb@gmail.com>
2023-10-14 22:33:17 -0300

    javascript code

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,7 +1,13 @@
-
-
-function clickButton() {
-    let input = document.querySelector(".input-search").value;
-
+function clickButton(evt) {
+  evt.preventDefault();
+    var input = document.querySelector(".input-search").value;
     console.log(input)
-}
+
+    axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search/?apikey=hdzcKMllZ4iOQzAg8NjbNb8kt7EYfBbW&q=${input}`)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+    }
